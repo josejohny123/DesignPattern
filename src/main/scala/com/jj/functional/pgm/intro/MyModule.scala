566c7fcd5d7bcfe3b41ecaa17cb23454561f66df
Jose Johny <Jose.Johny@EY.COM>
2020-08-14 17:27:47 +0530

    testing the project

---
+++ src/main/scala/com/jj/functional/pgm/intro/MyModule.scala
@@ -0,0 +1,16 @@
+package com.jj.functional.pgm.intro
+
+object MyModule {
+def abs(n:Int): Int ={
+  if(n<0) -n
+  else n
+  }
+  private def formatAbs(x: Int) = {
+    val msg = "the absolute value of %d is %d"
+    msg.format(x,abs(x))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatAbs(-42))
+  }
+}
